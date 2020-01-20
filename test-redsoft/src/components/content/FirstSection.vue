59e2e13c1b8fb54d91011470fd625b09a486b211
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2020-01-20 19:03:17 +0300

    c

--- test-redsoft/src/components/content/FirstSection.vue
+++ test-redsoft/src/components/content/FirstSection.vue
@@ -1,8 +1,9 @@
 <template>
   <section class="section1" style="background-image: url(img/sect1.png)">
-    <div class="heading heading--big">
+    <div class="heading">
       <h1 class="heading__title"> Расширяем возможности предоставления качественных медицинских услуг</h1>
       <p class="heading__sub-title"> «Дневник здоровья» – новый сервис для самоконтроля показателей состояния здоровья, разработанный для участников программы «Мое здоровье».</p>
+      <a href="" class="btn btn--heading">Узнать больше</a>
     </div>
   </section>
 </template>
@@ -15,4 +16,16 @@
   align-items: center
   justify-content: center
   background-position: center bottom
+.heading
+  max-width: 760px
+  margin: auto
+  text-align: center
+  color: #fff
+.heading__title
+  font-size: 32px
+  line-height: 1.6
+.heading__sub-title
+  max-width: 70%
+  margin: auto
+  line-height: 1.6
 </style>
